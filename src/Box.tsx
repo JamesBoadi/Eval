712ff0c3718c4efbda71ea070f90609c209cfbd5
JamesBoadi <39907481+JamesBoadi@users.noreply.github.com>
2022-02-04 14:19:58 +0000

    Update Box.tsx

--- src/Box.tsx
+++ src/Box.tsx
@@ -1,32 +1,32 @@
 import React, { useEffect } from "react";
+import List from "./List";
 
-export type Graph =
-{
-  Coordinates: Coordinates[];
-}
+export type Graph = {
+  Coordinates: Coordinates;
+};
 
-export interface Coordinates
-{
+export interface Coordinates {
   x: number;
   y: number;
 }
 
+//let list = new List<Graph>();
+//   let x = list.get().Coordinates.x;
+// let y = list.get().Coordinates.y;
+
 export default function Box() {
-  
   const renderBox = () => {
-     
-    
-
-  }
-
+    return <rect width="300" height="100" 
+    style={{ color: "yellow" }} />;
+  };
 
   return (
     <>
-    
-
+      <svg width="400" height="110">
+        {renderBox()}
+      </svg>
 
     </>
   );
 }
-
 
